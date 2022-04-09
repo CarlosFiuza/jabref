49889c1abb45fa3139fd4dc3192205cedd7d37ad
carlosFiuza <carlosfiuza1234@gmail.com>
2022-04-08 21:13:38 -0300

    Finalizando implementação funcionalidade BiodiversityLibraryFetcher e seus testes

--- src/main/java/org/jabref/logic/importer/fetcher/BiodiversityLibraryFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/BiodiversityLibraryFetcher.java
@@ -1,13 +1,27 @@
 package org.jabref.logic.importer.fetcher;
 
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
 import org.jabref.logic.importer.FetcherException;
+import org.jabref.logic.importer.ParseException;
 import org.jabref.logic.importer.Parser;
 import org.jabref.logic.importer.SearchBasedParserFetcher;
 import org.jabref.logic.importer.fetcher.transformers.DefaultQueryTransformer;
+import org.jabref.logic.util.OS;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.entry.types.StandardEntryType;
 
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BiodiversityLibraryFetcher implements SearchBasedParserFetcher {
 
@@ -18,14 +32,87 @@ public class BiodiversityLibraryFetcher implements SearchBasedParserFetcher {
 
     @Override
     public Parser getParser() {
-        return null;
+        return inputStream -> {
+            String response = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining(OS.NEWLINE));
+            JSONObject jsonObject = new JSONObject(response);
+
+            JSONArray jsonArray = jsonObject.getJSONArray("Result");
+            List<BibEntry> bibEntries = new ArrayList<>();
+
+            for (int index = 0; index < jsonArray.length(); index++) {
+                bibEntries.add(jsonItemToBibEntry(jsonArray.getJSONObject(index)));
+            }
+            return bibEntries;
+        };
     }
+
+    private BibEntry jsonItemToBibEntry(JSONObject item) throws ParseException {
+        BibEntry bibEntry = new BibEntry();
+
+        if (item.has("Genre")) {
+            String genre = item.getString("Genre");
+            if (genre.equals("Article")) {
+                bibEntry.setType(StandardEntryType.Article);
+            } else if (genre.equals("Book")) {
+                bibEntry.setType(StandardEntryType.Book);
+            }
+        }
+
+        if (item.has("Authors")) {
+            JSONArray authors = item.getJSONArray("Authors");
+            String authorsName = "";
+            for (int index = 0; index < authors.length(); index++) {
+                JSONObject author = authors.getJSONObject(index);
+                if (index > 0) {
+                    authorsName = authorsName.concat(", ");
+                }
+                authorsName = authorsName.concat(author.getString("Name"));
+            }
+            bibEntry.withField(StandardField.AUTHOR, authorsName);
+        }
+
+        if (item.has("BHLType")) {
+            String bhlType = item.getString("BHLType");
+            String url;
+            if (bhlType.equals("Part")) {
+                url = item.getString("PartUrl");
+                bibEntry.withField(StandardField.URL, url);
+            } else if (bhlType.equals("Item")) {
+                url = item.getString("ItemUrl");
+                bibEntry.withField(StandardField.URL, url);
+            }
+        }
+
+        if (item.has("Title")) {
+            String title = item.getString("Title");
+            bibEntry.withField(StandardField.TITLE, title);
+        }
+
+        if (item.has("Date")) {
+            String date = item.getString("Date");
+            bibEntry.withField(StandardField.YEAR, date);
+        }
+
+        if (item.has("Volume")) {
+            String volume = item.getString("Volume");
+            bibEntry.withField(StandardField.VOLUME, volume);
+        }
+
+        if (item.has("PageRange")) {
+            String pageRange = item.getString("PageRange");
+            bibEntry.withField(StandardField.PAGES, pageRange);
+        }
+
+        return bibEntry;
+    }
+
 
     @Override
     public URL getURLForQuery(QueryNode luceneQuery) throws URISyntaxException, MalformedURLException, FetcherException {
         URIBuilder uriBuilder = new URIBuilder(SEARCH_URL);
         String query = new DefaultQueryTransformer().transformLuceneQuery(luceneQuery).orElse("");
         uriBuilder.addParameter("searchterm", query);
+        uriBuilder.addParameter("format", "json");
         return uriBuilder.build().toURL();
     }
 
@@ -34,4 +121,3 @@ public class BiodiversityLibraryFetcher implements SearchBasedParserFetcher {
         return "Biodiversity Library";
     }
 }
-
--- src/test/java/org/jabref/logic/importer/fetcher/BiodiversityLibraryFetcherTest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/BiodiversityLibraryFetcherTest.java
@@ -2,11 +2,16 @@ package org.jabref.logic.importer.fetcher;
 
 import org.apache.lucene.queryparser.flexible.core.parser.SyntaxParser;
 import org.apache.lucene.queryparser.flexible.standard.parser.StandardSyntaxParser;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.entry.types.StandardEntryType;
 import org.jabref.testutils.category.FetcherTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 
 import static org.jabref.logic.importer.fetcher.transformers.AbstractQueryTransformer.NO_EXPLICIT_FIELD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,7 +30,7 @@ class BiodiversityLibraryFetcherTest {
         String testQuery = "test query url";
         SyntaxParser parser = new StandardSyntaxParser();
         URL url = fetcher.getURLForQuery(parser.parse(testQuery, NO_EXPLICIT_FIELD));
-        String expected = "https://www.biodiversitylibrary.org/api3?op=PublicationSearch&apikey=377e59ae-67f6-4228-8688-c4eb7187c4c9&searchterm=test+query+url";
+        String expected = "https://www.biodiversitylibrary.org/api3?op=PublicationSearch&apikey=377e59ae-67f6-4228-8688-c4eb7187c4c9&searchterm=test+query+url&format=json";
         assertEquals(expected, url.toString());
     }
 
@@ -35,4 +40,19 @@ class BiodiversityLibraryFetcherTest {
         String result = fetcher.getName();
         assertEquals(expected, result);
     }
+
+    @Test
+    void searchByQueryFindsEntry() throws Exception{
+        BibEntry searchEntry = new BibEntry(StandardEntryType.Article)
+                .withField(StandardField.AUTHOR, "Gifford, Edward Winslow,")
+                .withField(StandardField.URL, "https://www.biodiversitylibrary.org/part/69838")
+                .withField(StandardField.TITLE, "Field notes on the land birds of the Galapagos Islands, and of Cocos Island,Costa Rica")
+                .withField(StandardField.YEAR, "1919")
+                .withField(StandardField.VOLUME, "2")
+                .withField(StandardField.PAGES, "189--258");
+
+        List<BibEntry> fetchedEntries = fetcher.performSearch("cocos island costa rica birds");
+        assertEquals(Optional.of(searchEntry), fetchedEntries.stream().findFirst());
+    }
+
 }
